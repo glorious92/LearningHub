392bbee021148d4ce3fd601acfd2a3709725d6ce
ivo-7 <m.bahiraie92@gmail.com>
2024-08-07 22:42:00 -0400

    🟢 Update Project
    - Remove unnecessary files
    - Create To Do Components
    - Implement some simple style

--- ToDoList-React/todolist/src/App.js
+++ ToDoList-React/todolist/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="get-to-do">
+        <form>
+          <input type="text" placeholder="Enter Item"></input>
+          <button> Add To List</button>
+        </form>
+      </div>
+      {/* <div className="to-do-lists">
+        <ul>
+          <li> Item 1</li>
+          <li> Item 2</li>
+          <li> Item 3</li>
+        </ul>
+      </div> */}
     </div>
   );
 }
